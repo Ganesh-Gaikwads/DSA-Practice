add37fee9e2cf2c61834bab873b5fd4728f2d85a
Ganesh Gaikwad <theganeshgaikwad@gmail.com>
2025-10-07 18:45:40 +0530

    push pending file of challenge

---
+++ daily-challenge/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+
+// function maxNum(arr){
+//   let max = [0];
+//   for(let i=0; i<arr.length; i++){
+//     if(max<arr[i]){
+//       max = arr[i]
+//     }
+//   }
+//   return `max number in Array ${max}`;
+// }
+// console.log(maxNum([23,43,65,34,56]));
